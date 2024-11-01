41297a730aea1d7c4c9248201ddf1241dabc0934
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2024-11-01 06:38:17 +0530

    Add files via upload

--- demo/src/main/resources/static/script.js
+++ demo/src/main/resources/static/script.js
@@ -17,9 +17,24 @@
     const locZ = data.get("locZ");
     const destX = data.get("destX");
     const destZ = data.get("destZ");
-
+    const netherEquivCurrent=document.querySelector('#netherEquivCurrent').checked;
+    const netherEquivDest = document.querySelector('#netherEquivDest').checked;
+    if(netherEquivCurrent)
+    {
+      locX /= 8;
+      locX = (Math.round (locX * 1000.0)) / 1000.0;
+      locZ /= 8;
+      locZ = (Math.round (locZ * 1000.0)) / 1000.0;
+    }
+    if(netherEquivDest)
+      {
+        destX /= 8;
+        destX = (Math.round (destX * 1000.0)) / 1000.0;
+        destZ /= 8;
+        destZ = (Math.round (destZ * 1000.0)) / 1000.0;
+      }
     // Build the url
-   const url = new URL(`${window.location.href}` + "calculate");
+    const url = new URL(`${window.location.href}` + "calculate");
     url.searchParams.append("xcurrent", locX);
     url.searchParams.append("zcurrent", locZ);
     url.searchParams.append("xdest", destX);
